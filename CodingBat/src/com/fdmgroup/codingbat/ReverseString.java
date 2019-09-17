3e9c5b4e6a28d0859cb4e20f90932121fa2a65b7
Sarah.Horng <Sarah.Horng@TRNNYC30071.fdmgroup.local>
2019-09-17 17:01:38 -0400

    changed the reverse string to have a scanner

--- CodingBat/src/com/fdmgroup/codingbat/ReverseString.java
+++ CodingBat/src/com/fdmgroup/codingbat/ReverseString.java
@@ -1,17 +1,21 @@
 package com.fdmgroup.codingbat;
 
+import java.util.Scanner;
+
 public class ReverseString {
 	public static void main(String[] args) {
-		String original = "Sarah";
+		
+		System.out.println("Please enter String you want to Reverse: ");
+		
+		Scanner scan = new Scanner(System.in);
+		String original = scan.nextLine();		
+		
 		String rev = "";
 
 		for (int i = original.length()-1; i >= 0; i--) {
 			rev = rev + original.charAt(i);
 		}
-		System.out.println(rev);
+		System.out.println("Reversed String: " + rev);
 	}
 
 }
-
-
-
